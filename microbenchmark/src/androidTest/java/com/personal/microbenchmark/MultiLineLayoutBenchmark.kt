944d0847c8ecb4788ceba75661930391e024ac68
empty <empty>
2022-06-05 19:03:24 +0800

    多行布局性能测试

--- microbenchmark/src/androidTest/java/com/personal/microbenchmark/MultiLineLayoutBenchmark.kt
+++ microbenchmark/src/androidTest/java/com/personal/microbenchmark/MultiLineLayoutBenchmark.kt
@@ -5,24 +5,19 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.OrderWith
 import org.junit.runner.RunWith
+import org.junit.runner.manipulation.Alphanumeric
 
 /**
  * 多行View展示，用于验证布局排列对于性能影响
  */
 @LargeTest
 @RunWith(AndroidJUnit4::class)
+@OrderWith(Alphanumeric::class)
 class MultiLineLayoutBenchmark {
     @get:Rule
     val benchmarkRule = BenchmarkRule()
-
-    @Test
-    fun linearLayout_at_most_measure() {
-        LayoutPerformanceUtils.analysisLayoutMeasureAndLayout(
-            benchmarkRule,
-            R.layout.multi_line_linearlayout_at_most
-        )
-    }
 
     @Test
     fun linearLayout_exactly_measure() {
@@ -33,26 +28,10 @@ class MultiLineLayoutBenchmark {
     }
 
     @Test
-    fun relativeLayout_at_most_measure() {
-        LayoutPerformanceUtils.analysisLayoutMeasureAndLayout(
-            benchmarkRule,
-            R.layout.multi_line_relative_layout_at_most
-        )
-    }
-
-    @Test
     fun relativeLayout_exactly_measure() {
         LayoutPerformanceUtils.analysisLayoutMeasureAndLayout(
             benchmarkRule,
             R.layout.multi_line_relativelayout_exactly
-        )
-    }
-
-    @Test
-    fun constraintLayout_at_most_measure() {
-        LayoutPerformanceUtils.analysisLayoutMeasureAndLayout(
-            benchmarkRule,
-            R.layout.multi_line_constraintlayout_at_most
         )
     }
 
